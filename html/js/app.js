ffd6e1be293ac452e391b56f1702277c7a26da62
Adam Zolyak <adam.zolyak@gmail.com>
2017-04-23 13:35:04 -0600

    Add checkType

--- html/js/app.js
+++ html/js/app.js
@@ -1,17 +1,22 @@
 var pipelines = [
-		"https://jsonplaceholder.typicode.com/posts/1",
-		"https://jsonplaceholder.typicode.com/posts/2",
-		"https://jsonplaceholder.typicode.com/posts/3",
-		"https://jsonplaceholder.typicode.com/posts/4",
-		"https://jsonplaceholder.typicode.com/posts/5",
+		{url:"https://jsonplaceholder.typicode.com/posts/1", checkType:"poorHealth"},
+		{url:"https://jsonplaceholder.typicode.com/posts/2", checkType:"poorHealth"},
+		{url:"https://jsonplaceholder.typicode.com/posts/3", checkType:"singleFail"},
+		{url:"https://jsonplaceholder.typicode.com/posts/4", checkType:"singleFail"},
+		{url:"https://jsonplaceholder.typicode.com/posts/5", checkType:"singleFail"},
 	];
 
-function getAPI(url) {
+function getAPI(url, checkType) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 			var responseObject = JSON.parse(xhr.responseText)
-			document.getElementById('result').innerHTML += (responseObject.id + '<br>');
+
+			if (checkType === "poorHealth") {
+				document.getElementById('result').innerHTML += ("Poor Health Check: " + responseObject.id + '<br>');
+			} else if (checkType === "singleFail") {
+				document.getElementById('result').innerHTML += ("Single Fail Check: " + responseObject.id + '<br>');
+			}
 		}
 	};
 	xhr.open('GET', url);
@@ -20,7 +25,7 @@ function getAPI(url) {
 
 function checkHealth(updateHealthReport) {
 	for (var i=0; i<pipelines.length; i++) {
-		getAPI(pipelines[i]);
+		getAPI(pipelines[i].url,pipelines[i].checkType);
 	}	
 }
 
