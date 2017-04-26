1638c4f6a55e365257511b3f11b836e303732ea4
Adam Zolyak <adam.zolyak@gmail.com>
2017-04-25 21:46:29 -0600

    Refactor w JQuery AJAX

--- html/js/app.js
+++ html/js/app.js
@@ -1,22 +1,9 @@
-function getPipelines() {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			var responseObject = JSON.parse(xhr.responseText)
-			checkHealth(responseObject);
-		}
-	};
-	xhr.open('GET', 'pipelines.json');
-	xhr.send();
-
-}
-
 function addResult(pipelineName, pipelineLeakiness) {
 	document.getElementById('results').innerHTML += (`<li class="result">${pipelineName} is ${pipelineLeakiness}% leaky</li>`);
 }
 
-function checkResult(pipelineName, responseJSON) {
-	pipelineLeakiness = 100 - responseJSON.healthReport[0].score;
+var checkResult = function (pipelineName, healthReportScore) {
+	pipelineLeakiness = 100 - healthReportScore;
 
 	if (pipelineLeakiness >= 80) {
 		addResult(pipelineName,pipelineLeakiness);
@@ -26,23 +13,15 @@ function checkResult(pipelineName, responseJSON) {
 	} 
 }
 
-function getPipeline(pipelineName, url) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			var responseObject = JSON.parse(xhr.responseText)
-			checkResult(pipelineName, responseObject);
-		}
-	};
-	xhr.open('GET', url);
-	xhr.send();
+var getPipeline = function (index, pipeline) {
+	$.getJSON(pipeline.url,function (response) {
+		checkResult(pipeline.pipelineName, response.healthReport[0].score);
+	})
+	
 }
 
-function checkHealth(pipelines) {
-	console.log("Pipelines: " + pipelines);
-	for (var i=0; i<pipelines.length; i++) {
-		getPipeline(pipelines[i].pipelineName,pipelines[i].url);
-	}			
+var checkAllPipelineHealth = function (pipelines) {
+	$.each(pipelines, getPipeline)		
 }
 
-$.ready(getPipelines());
+$.ready($.getJSON("pipelines.json",checkAllPipelineHealth));
